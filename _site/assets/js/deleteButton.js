e47f7e53b61a8441191a24a6df9722f7e642ec49
Nilo Pedrazzini <nilo.pedrazzini@gmail.com>
2023-07-28 13:31:56 +0100

    remove site

--- _site/assets/js/deleteButton.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  (document.querySelectorAll('.iiif-notification .delete') || []).forEach(($delete) => {
-    const $notification = $delete.closest(".iiif-notification");
-
-    $delete.addEventListener('click', () => {
-      $notification.parentNode.removeChild($notification);
-    });
-  });
-});
